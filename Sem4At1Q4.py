80709eca395df36ed14acf847ae46e9bcf8ee8ba
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-03-08 09:58:14 -0300

    Add files via upload

---
+++ Sem4At1Q4.py
@@ -0,0 +1,6 @@
+horas = int(input())
+minutos = int(input())
+segundos = int(input())
+h = horas*3600
+m = minutos * 60
+print(f"{h+m+segundos}")
---
+++ Sem4At1Q5.py
@@ -0,0 +1,9 @@
+altura = float(input())
+comprimento = float(input())
+largura = float(input())
+a_piso_s = largura*comprimento
+v_s = largura*comprimento*altura
+a_parede_s = (2*altura*largura) + (2*altura*comprimento)
+print(a_piso_s)
+print(v_s)
+print(a_parede_s)
