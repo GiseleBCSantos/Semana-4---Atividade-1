231eeaba2a9ed516d8140a12bf7376d08973ee80
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-03-08 14:28:51 -0300

    Update Sem4At1Q4.py

--- Sem4At1Q4.py
+++ Sem4At1Q4.py
@@ -1,6 +1,6 @@
-horas = int(input())
-minutos = int(input())
-segundos = int(input())
+horas = int(input("Insira a quantidade de horas atual: "))
+minutos = int(input("Insira a quantidade de minutos atual: "))
+segundos = int(input("Insira a quantidade de segundos atual: "))
 h = horas*3600
 m = minutos * 60
-print(f"{h+m+segundos}")
+print(f"Passou um total de {h+m+segundos} desde a meia noite.")
