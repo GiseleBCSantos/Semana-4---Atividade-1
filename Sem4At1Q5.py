ba5bd79f5027fa3f827d8a2191469e693e1cd839
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-03-08 14:33:15 -0300

    Update Sem4At1Q5.py

--- Sem4At1Q5.py
+++ Sem4At1Q5.py
@@ -1,9 +1,9 @@
-altura = float(input())
-comprimento = float(input())
-largura = float(input())
+altura = float(input("Insira a altura da parede em metros: "))
+comprimento = float(input("Insira o comprimento da parede em metros: "))
+largura = float(input("Insira a largura da parede em metros: "))
 a_piso_s = largura*comprimento
 v_s = largura*comprimento*altura
 a_parede_s = (2*altura*largura) + (2*altura*comprimento)
-print(a_piso_s)
-print(v_s)
-print(a_parede_s)
+print(f"Área do piso da sala: {a_piso_s}m².")
+print(f"Volume da sala: {v_s}m³.")
+print(f"Área das paredes da sala: {a_parede_s}m².")
